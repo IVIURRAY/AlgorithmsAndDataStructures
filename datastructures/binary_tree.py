b6ec1205b2bd89cafb1b35ef6fe7201c36c85ee3
IVIURRAY <haydn.murray1@yahoo.co.uk>
2020-04-25 12:34:27 +0100

    Revert "Add files via upload"
    This reverts commit cf3603878d4f0284cbf8c6500d0c217033b39269.

--- datastructures/binary_tree.py
+++ datastructures/binary_tree.py
@@ -1,142 +1,193 @@
-class Node:
-    def  __init__(self, val):
-        self.val = val
+"""
+Binary tree representation
+"""
+
+class Node(object):
+
+    def __init__(self, data=None):
+        self.data = data
         self.left = None
         self.right = None
 
-    def __str__(self):
-        return str(self.val)
-
     def __repr__(self):
-        return str(self.val)
+        return str(self.data)
 
-class BinarySearchTree:
-    def __init__(self):
-        self.root = None
-        self.result = []
-
-    def insert(self, value):
-        if not self.root:
-            self.root = Node(value)
-            return
-
-        node = self.root
-        new_node = Node(value)
-        while node.val is not None:
-            if value > node.val:
-                if not node.right:
-                    node.right = new_node
-                    break
-                else:
-                    node = node.right
-            else:
-                if not node.left:
-                    node.left = new_node
-                    break
-                else:
-                    node = node.left
+    def __eq__(self, other):
+        return self.data == other
 
 
-def pre_order_recursion(root):
-    result = []
+n1 = Node(10)
+n2 = Node(5)
+n3 = Node(20)
+n4 = Node(9)
+n5 = Node(18)
+n6 = Node(3)
+n7 = Node(7)
 
-    if root:
-        result.append(root)
-        result = result + pre_order_recursion(root.left)
-        result = result + pre_order_recursion(root.right)
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+n3.left = n6
+n3.right = n7
 
-    return result
+"""
+Level, Nodes
+1:          10
+          /     \
+2:      5        20
+      /   \     /   \
+3:  9      18  3     7
+"""
 
 
-def in_order_recursion(root):
-    result = []
-    if root:
-        result = in_order_recursion(root.left)
-        result.append(root.val)
-        result = result + in_order_recursion(root.right)
+def visit(node):
+    if node:
+        print(node.data)
 
-    return result
 
-def post_order_recursion(root):
-    result = []
+def in_order_traversal(root):
+    if root is not None:
+        in_order_traversal(root.left)
+        visit(root)
+        in_order_traversal(root.right)
 
-    if root:
-        result = post_order_recursion(root.left)
-        result = result + post_order_recursion(root.right)
-        result.append(root)
 
-    return result
+def pre_order_traversal(root):
+    if root is not None:
+        visit(root)
+        in_order_traversal(root.left)
+        in_order_traversal(root.right)
 
-def pre_order_iterative(root):
-    stack = []
-    result =  []
-    node = root
 
-    while stack or node:
-        if node:
-            result.append(node)
-            stack.append(node)
-            node = node.left
-        else:
-            node = stack.pop()
-            node = node.right
+def post_order_traversal(root):
+    if root is not None:
+        in_order_traversal(root.left)
+        in_order_traversal(root.right)
+        visit(root)
 
-    return result
 
-def in_order_iterative(root):
-    stack = []
-    result = []
-    node = root
+print('In order:')
+in_order_traversal(n1)
+print('Pre order')
+pre_order_traversal(n1)
+print('Post order')
+post_order_traversal(n1)
 
-    while stack or node:
-        if node:
-            stack.append(node)
-            node = node.left
-        else:
-            node = stack.pop()
-            result.append(node)
-            node = node.right
 
-    return result
+def dfs_simple(root):
+    if root is None:
+        return
 
-def post_order_iterative(root):
-    stack = []
-    result = []
-    node = root
+    visit(root)
+    if root.left:
+        dfs_simple(root.left)
+    if root.right:
+        dfs_simple(root.right)
 
-    while stack or node:
-        if node:
-            stack.append(node)
-            result = [node] + result
-            node = node.right
-        else:
-            node = stack.pop()
-            node = node.left
 
-    return result
+def dfs_depth_of_target_at_first_occurrence(root, target):
+    if root is None:
+        return 0
 
-bst = BinarySearchTree()
-bst.insert(20)
-bst.insert(9)
-bst.insert(25)
-bst.insert(5)
-bst.insert(12)
-bst.insert(11)
-bst.insert(14)
+    if root == target:
+        return 1
 
-'''
-        20
-       /   \
-     9      25
-   /  \
-  5    12
-      /   \
-     11   14
-'''
-print(f'PreOrder    - Recursion: {pre_order_recursion(bst.root)}')
-print(f'InOrder     - Recursion: {in_order_recursion(bst.root)}')
-print(f'PostOrder   - Recursion: {post_order_recursion(bst.root)}')
+    left_dfs = dfs_depth_of_target_at_first_occurrence(root.left, target)
+    right_dfs = dfs_depth_of_target_at_first_occurrence(root.right, target)
 
-print(f'PreOrder    - Iterative: {pre_order_iterative(bst.root)}')
-print(f'InOrder     - Iterative: {in_order_iterative(bst.root)}')
-print(f'PostOrder   - Iterative: {post_order_iterative(bst.root)}')
+    depth = max(left_dfs, right_dfs)
+    return depth + 1 if depth else depth
+
+
+print('dfs')
+dfs_simple(n1)
+print('dfs with target')
+print(dfs_depth_of_target_at_first_occurrence(n1, 10))
+print(dfs_depth_of_target_at_first_occurrence(n1, 5))
+print(dfs_depth_of_target_at_first_occurrence(n1, 18))
+print(dfs_depth_of_target_at_first_occurrence(n1, 3))
+
+
+def bfs_simple(root):
+    q = [root]
+    while q:
+        n = q.pop(0)
+        visit(n)
+        if n.left is not None:
+            q.append(n.left)
+        if n.right is not None:
+            q.append(n.right)
+
+def bfs_is_target_in(root, target):
+    q = [root]
+    while q:
+        n = q.pop(0)
+        if n == target:
+            return True
+        visit(n)
+        if n.left is not None:
+            q.append(n.left)
+        if n.right is not None:
+            q.append(n.right)
+
+    return False
+
+
+def bfs_is_target_in_return_node(root, target):
+    q = [root]
+    while q:
+        n = q.pop(0)
+        if n == target:
+            return n
+        visit(n)
+        if n.left is not None:
+            q.append(n.left)
+        if n.right is not None:
+            q.append(n.right)
+
+    return Node()
+
+print('bfs')
+bfs_simple(n1)
+print('bfs with target')
+print(bfs_is_target_in(n1, 101))
+print(bfs_is_target_in(n1, 5))
+
+def route_between_two_nodes(root, a, b):
+    if a == b:
+        return True
+    q = [root]
+
+    while q:
+        n = q.pop(0)
+        if n == a:
+            a_children = [n]
+            while a_children:
+                child = a_children.pop(0)
+                if child == b:
+                    return True
+                if child.left is not None:
+                    a_children.append(child.left)
+                if child.right is not None:
+                    a_children.append(child.right)
+
+        if n.left is not None:
+            q.append(n.left)
+        if n.right is not None:
+            q.append(n.right)
+
+    return False
+
+
+def route_between_two_nodes2(root, a, b):
+    a_tree = bfs_is_target_in_return_node(root, a)
+    if a_tree:
+        return bfs_is_target_in(a_tree, b)
+
+    return False
+
+
+print('Route between two nodes')
+print(route_between_two_nodes(n1, Node(5), Node(18)))
+print(route_between_two_nodes2(n1, Node(5), Node(18)))
